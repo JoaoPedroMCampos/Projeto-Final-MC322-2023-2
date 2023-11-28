5c76cd953d7b85bebbb038349f513b2d5c584d83
quantsr <max.a.braz@gmail.com>
2023-11-28 14:31:35 -0300

    mudança de atributo int cnpj para String cnpj

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 import agencia.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Debug");
+        //instanciamento de objeto de Agencia quando concertarem a classe
+        Agencia agencia = new Agencia("66320453000103", "Agencia de Turismo", 99999999, "email@exemplo.com", null, null, null, null, null);
+
+        Scanner input = new Scanner(System.in);
     }
     
 }
