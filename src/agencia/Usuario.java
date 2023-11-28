7174d09511f922292da9d82979ef3371c0dc1e80
JoaoPedroMCampos <142697433+JoaoPedroMCampos@users.noreply.github.com>
2023-11-28 08:41:07 -0300

    Correçoes classe Usuario.java

--- src/agencia/Usuario.java
+++ src/agencia/Usuario.java
@@ -1,86 +1,88 @@
 package agencia;
+
 import java.time.LocalDate;
 import java.util.LinkedList;
 
 public class Usuario {
 
-	//atributos da classe
-	private String login;
-	private String senha;
-	private String nome;
-	private int cpf;
-	private int telefone;
-	private String email;
-	private LocalDate nascimento;
-	private LinkedList<Pacote> listaPacotes;
-					
-	//construtor da classe
-	public Usuario(String login, String senha, String nome, int cpf, int telefone, String email, LocalDate nascimento) {
-		this.login = login;
-		this.senha = senha;
-		this.nome = nome;
-		this.cpf = cpf;
-		this.telefone = telefone;
-		this.email = email;
-		this.nascimento = nascimento;
-	}
-	
-	//Metodos da classe
-		
-	//Getters para acessar os atributos privado
-	public String getLogin() {
-		return login;
-	}
-	
-	public String getSenha() {
-		return senha;
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	
-	public int getCpf() {
-		return cpf;
-	}
-	
-	public int getTelefone() {
-		return telefone;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	
-	public LocalDate getNascimento() {
-		return nascimento;
-	}
-	
-	public String setLogin() {
-		return login;
-	}
-	
-	public String setSenha() {
-		return senha;
-	}
-	
-	public String setNome() {
-		return nome;
-	}
-	
-	public int setCpf() {
-		return cpf;
-	}
-	
-	public int setTelefone() {
-		return telefone;
-	}
-	
-	public String setEmail() {
-		return email;
-	}
-	
-	public LocalDate setNascimento() {
-		return nascimento;
-	}			
+    // atributos da classe
+    private String login;
+    private String senha;
+    private String nome;
+    private int cpf;
+    private int telefone;
+    private String email;
+    private LocalDate nascimento;
+    private LinkedList<Pacote> listaPacotes;
+
+    // construtor da classe
+    public Usuario(String login, String senha, String nome, int cpf, int telefone, String email, LocalDate nascimento) {
+        this.login = login;
+        this.senha = senha;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+        this.nascimento = nascimento;
+    }
+
+    // Métodos da classe
+
+    // Getters para acessar os atributos privados
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    // Métodos setters devem aceitar parâmetros
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
 }
